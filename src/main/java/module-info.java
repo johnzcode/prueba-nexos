4fb28d90a78d8eb86d4972784692415865b35a63
johnz <johnzcode@gmail.com>
2024-08-24 17:36:54 -0500

    elimino dependencias innecesarias, cambio version java 22 a 17 ya que daba problemas para configurar el wildfly y agrego mensaje de prueba

--- src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module org.example.pruebanexos {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens org.example.pruebanexos to javafx.fxml;
-    exports org.example.pruebanexos;
-}
---
+++ src/main/java/org/example/pruebanexos/Servidor/Servlet.java
@@ -0,0 +1,19 @@
+package org.example.pruebanexos.Servidor;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/home")
+public class Servlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        response.getWriter().write("<h1>Hola mundo</h1>");
+    }
+}
